29b10e551bb4e01dca6dc3db8e73a4ec68360e94
Koblan <kokesept1@gmail.com>
2024-10-15 03:52:29 +0500

    HomeWork 5

---
+++ main.js
@@ -0,0 +1,60 @@
+//Основы JavaScript
+/*let a, b, c;
+a = b = c = 20;
+const d = 100;*/
+/*let e = 1000;
+const d = 500;
+let a = 322;*/
+//let $ye = 600;
+
+//Типы данных
+/*let number, bigint, string, boolean;
+number = 140;
+bigint = 1000000000000n;
+string = "Hello man!";
+boolean = true;
+console.log(typeof number);
+console.log(typeof bigint);
+console.log(typeof string);
+console.log(typeof boolean);
+
+let z = 500,
+  r = -200,
+  j = "kek";
+console.log(z / 0);
+console.log(r / 0);
+console.log(z / 0);
+console.log(j / 0);
+
+let der = "bread",
+  ger = "milk",
+  yer = `juice`;
+console.log(ger + `${yer + 10 + der}`);
+
+let far;
+let dar = null;
+console.log(typeof far);
+console.log(typeof dar);
+
+let num = 1000;
+let word = "bike";
+console.log(typeof String(num));
+console.log(typeof (num > word));*/
+
+alert("Привет!");
+prompt("Как тебя зовут?");
+confirm("Ты уверен в своем ответе?");
+alert("Вы уверены, молодец!");
+
+//Операторы
+let x = 10;
+x += 5;
+let y = 3;
+let z = x ** y;
+let isGreaterThan1000 = z > 1000;
+let complexCondition = x > y && z < 10000;
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(isGreaterThan1000);
+console.log(complexCondition);
